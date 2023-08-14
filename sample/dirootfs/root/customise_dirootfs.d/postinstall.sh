e14c7f1eb01a7fd80b339c6684068ee52923da41
Hayate Nakamura <is01.njb@gmail.com>
2023-08-14 23:38:50 +0900

    更に挙動を変更

--- sample/dirootfs/root/customise_dirootfs.d/postinstall.sh
+++ sample/dirootfs/root/customise_dirootfs.d/postinstall.sh
@@ -1,1 +1,3 @@
-ln -fs /etc/machine-id /var/lib/dbus/machine-id
+ln -sf /etc/machine-id /var/lib/dbus/machine-id
+rm /etc/systemd/system/display-manager.service
+ln -sf /usr/lib/systemd/system/gdm3.service /etc/systemd/system/display-manager.service
