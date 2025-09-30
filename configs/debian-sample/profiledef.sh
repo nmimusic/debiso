02b77fc0d465313ff7266c065ad6a0dd48fef8ad
Jin Asanami <hayatehay.njb@gmail.com>
2025-09-30 19:30:14 +0900

    パッケージファイルを3分割

--- configs/debian-sample/profiledef.sh
+++ configs/debian-sample/profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # License: 3-clause BSD
-DISTRO_NAME="Debian H.A.Y. Remix"
-DISTRO_UNAME="debian-hay-remix"
+DISTRO_NAME="Debian Remix"
+DISTRO_UNAME="debian-remix"
 DISTRO_VERSION="$(date +%y%m%d)"
 UPSTREAM="debian"
-UPSTREAM_VERSION="bookworm"
+UPSTREAM_VERSION="trixie"
 MIRROR_URL="https://deb.debian.org/debian"
 ARCH="amd64"
--- configs/ubuntu-sample/profiledef.sh
+++ configs/ubuntu-sample/profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # License: 3-clause BSD
-DISTRO_NAME="Ubuntu H.A.Y. Remix"
-DISTRO_UNAME="ubuntu-hay-remix"
+DISTRO_NAME="Ubuntu Remix"
+DISTRO_UNAME="ubuntu-remix"
 DISTRO_VERSION="$(date +%y%m%d)"
 UPSTREAM="ubuntu"
-UPSTREAM_VERSION="jammy"
+UPSTREAM_VERSION="noble"
 MIRROR_URL="http://archive.ubuntu.com/ubuntu"
 ARCH="amd64"
